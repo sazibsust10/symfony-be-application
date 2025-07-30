0aa27ec3c6324afde5e16e9f9dfb9d4771115b22
M M Mahmudul Hassan <sazibsust10@gmail.com>
2025-07-30 11:18:54 +0200

    Refactor Dockerfile and add configuration files for Symfony application
    - Updated Dockerfile to install necessary dependencies and composer.
    - Added framework.yaml and services.yaml for Symfony configuration.
    - Created Kernel.php to set up the application kernel with container and routing configuration.
    - Improved index.php to handle environment variables more robustly.

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM php:8.2-fpm
 
+# Install dependencies
 RUN apt-get update && apt-get install -y \
     git unzip libicu-dev libonig-dev libzip-dev zip curl \
     && docker-php-ext-install intl pdo pdo_mysql zip
 
+# Install composer
 COPY --from=composer:2 /usr/bin/composer /usr/bin/composer
 
 WORKDIR /var/www
--- public/index.php
+++ public/index.php
@@ -1,19 +1,18 @@
 <?php
+
 use App\Kernel;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-if ($_SERVER['APP_DEBUG']) {
+if ($_SERVER['APP_DEBUG'] ?? false) {
     umask(0000);
-
     Debug::enable();
 }
 
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$kernel = new Kernel($_SERVER['APP_ENV'] ?? 'prod', (bool) ($_SERVER['APP_DEBUG'] ?? false));
 $request = Request::createFromGlobals();
-
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
